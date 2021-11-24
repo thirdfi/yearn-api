7784fd2e8992b6e7d01b24406bd07da52b8ab4b5
yashthakor <yashthakor4595@gmail.com>
2021-11-24 17:24:42 +0530

    reverted log

--- config/db.js
+++ config/db.js
@@ -33,7 +33,7 @@ const connectDB = async (callback) => {
           JSON.stringify(err)
         );
       } else {
-        console.log("Connected successfully to database1");
+        console.log("Connected successfully to database");
         _db = client.db(MONGO_DB, { useUnifiedTopology: true });
       }
 
