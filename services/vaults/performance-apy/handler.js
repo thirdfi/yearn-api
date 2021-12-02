cc462f1eea30a0ce068b53928fa28d426feeb467
Phyllice Wai <waikahkwan0903@gmail.com>
2021-12-02 12:38:37 +0800

    feat: Chart Series

--- services/vaults/performance-apy/handler.js
+++ services/vaults/performance-apy/handler.js
@@ -108,6 +108,12 @@ const getApyAttributeNameByStrategy = (strategyType) => {
                 { seriesName: "BTC", attributeName: "btc_performance" },
                 { seriesName: "ETH", attributeName: "eth_performance" },
             ];
+        case constant.STRATEGY_TYPE.BNB2X:
+            return [
+                { seriesName: "Leverage BNB", attributeName: "lp_performance"},
+                { seriesName: "BTC", attributeName: "btc_performance" },
+                { seriesName: "ETH", attributeName: "eth_performance" },
+            ];
         default: 
             return [];
     }
